dd9eb3cfe7a720b851d5a9daaf75a67cd3a60f85
AIDIL 'AQIF MOHD PIDAUS <92839227+aidilaqif@users.noreply.github.com>
2024-08-20 13:33:19 +0800

    Update qrscanner.py

--- qrscanner.py
+++ qrscanner.py
@@ -14,6 +14,7 @@ import time
 #     print("Error opening video stream or file")
 #     exit() # Exit if camera cannot be opened
 
+# video variable
 cam = cv2.VideoCapture(0)
 
 # window reflected when camera is true
